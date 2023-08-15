c2d3a995264a5f4f5a8b6e823ba05548827a86d3
ashustikov@kameleoon.com <ashustikov@kameleoon.com>
2023-08-15 16:18:05 -0400

    #1

--- src/test/java/ru/acorn/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/ru/acorn/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package ru.acorn.reactivespring.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -15,6 +16,18 @@ public class FluxAndMonoTest {
         stringFlux
                 .subscribe(System.out::println,
                         (e) -> System.err.println("Exception is " + e));
+    }
+
+    @Test
+    public void fluxTestElements_WithoutError(){
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+                .verifyComplete();
 
     }
 }
